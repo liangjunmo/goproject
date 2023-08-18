2578e10174bb943cd008164258596f1be7f67f01
liangjunmo <liangjunmo@foxmail.com>
2023-08-18 17:35:49 +0800

    add init sql

---
+++ data/init.sql
@@ -0,0 +1,4 @@
+# test / test
+INSERT INTO `user` (`id`, `create_time`, `update_time`, `delete_time`, `username`, `password`)
+VALUES
+    (1, '2023-08-18 00:00:00', '2023-08-18 00:00:00', NULL, 'test', '$2a$10$NwcZY8UXUTvvi.DBHZ9AcObmf/EA0ixdttjarQrCsUQejlUIL63la');
