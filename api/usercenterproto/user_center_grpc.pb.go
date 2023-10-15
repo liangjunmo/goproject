eaac2cd19d1c108ed0c430a278d3e2fed113eaa3
liangjunmo <liangjunmo@foxmail.com>
2023-10-15 21:17:39 +0800

    refactor usercenterproto

--- api/usercenterproto/user_center_grpc.pb.go
+++ api/usercenterproto/user_center_grpc.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // - protoc-gen-go-grpc v1.2.0
 // - protoc             v4.24.4
-// source: api/usercenterproto/usercenter.proto
+// source: api/usercenterproto/user_center.proto
 
 package usercenterproto
 
@@ -245,5 +245,5 @@ var UserCenter_ServiceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "api/usercenterproto/usercenter.proto",
+	Metadata: "api/usercenterproto/user_center.proto",
 }
