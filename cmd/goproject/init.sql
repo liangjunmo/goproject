f77e1563da3a28a2db3ba172d4b535dd3fdbabba
liangjunmo <liangjunmo@foxmail.com>
2024-01-09 19:07:46 +0800

    move all init.sql to data dir

--- cmd/goproject/init.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO `user` (`id`, `create_time`, `update_time`, `delete_time`, `uid`) VALUES (1, '2023-08-18 00:00:00', '2023-08-18 00:00:00', NULL, 1);
--- data/init.sql
+++ data/init.sql
@@ -1,2 +1,3 @@
 # test / test
 INSERT INTO `usercenter_user` (`id`, `create_time`, `update_time`, `delete_time`, `username`, `password`) VALUES (1, '2023-08-18 00:00:00', '2023-08-18 00:00:00', NULL, 'test', '$2a$10$NwcZY8UXUTvvi.DBHZ9AcObmf/EA0ixdttjarQrCsUQejlUIL63la');
+INSERT INTO `user` (`id`, `create_time`, `update_time`, `delete_time`, `uid`) VALUES (1, '2023-08-18 00:00:00', '2023-08-18 00:00:00', NULL, 1);
